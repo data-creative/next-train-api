7654e6b8992dd082e69bad72003057f64c703a86
MJ Rossetti <datacreativellc@gmail.com>
2018-12-27 15:25:12 -0500

    Fix failing test

--- spec/mailers/gtfs_import_mailer_spec.rb
+++ spec/mailers/gtfs_import_mailer_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe GtfsImportMailer, type: :mailer do
         let(:subject) { "GTFS Schedule Activation!" }
         let(:results_keys) { [ :source_url, :start_at, :end_at, :destructive, :hosted_schedule, :schedule_activation ] }
         let(:expected_text){ [
-          subject, "Hosted schedule:"
+          subject, "Source URL:", "Hosted Schedule:"
         ] }
       end
     end
@@ -24,7 +24,7 @@ RSpec.describe GtfsImportMailer, type: :mailer do
         let(:subject) { "GTFS Schedule Verification" }
         let(:results_keys) { [ :source_url, :start_at, :end_at, :destructive, :hosted_schedule, :schedule_verification ] }
         let(:expected_text){ [
-          subject, "Hosted schedule:"
+          subject, "Source URL:", "Hosted Schedule:"
         ] }
       end
     end
@@ -37,7 +37,7 @@ RSpec.describe GtfsImportMailer, type: :mailer do
         let(:subject) { "GTFS Schedule Processing Failure" }
         let(:results_keys) { [ :source_url, :start_at, :end_at, :destructive, :errors] }
         let(:expected_text){ [
-          subject, "Errors:"
+          subject, "Source URL:", "Errors:"
         ] }
       end
     end
